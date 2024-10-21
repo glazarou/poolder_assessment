cfe9d16afe01bed596c8666e67458a82c7333587
GIANNIS LAZAROU <g.lazarou@student.tudelft.nl>
2024-10-21 17:25:23 +0200

    Initial commit

---
+++ src/middleware_test.ts
@@ -0,0 +1,35 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req:NextRequest){
+    const res = NextResponse.next();
+
+    const supabase = createMiddlewareClient({req,res});
+
+    const {
+        data: {
+            session
+        }
+    } = await supabase.auth.getSession();
+
+    console.log(session)
+    
+    if (!session) {
+        return NextResponse.rewrite(new URL('/login', req.url))
+    }
+
+    return res
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico (favicon file)
+       */
+      '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+  }
